3443c6ce1884b64b3a9aa5c097825d7e3607cd4f
michelezaccagnini <michelezaccagnini@users.noreply.github.com>
2024-04-02 09:48:04 -0400

    drum sequencer challenge

--- Assets/Scripts/SequencerDrum.cs
+++ Assets/Scripts/SequencerDrum.cs
@@ -11,12 +11,36 @@ public class SequencerDrum : MonoBehaviour
     int[] mode;
     int count = 0;
     
+
     [SerializeField]
     List<bool> kick;
     [SerializeField]
     List<bool> snare;
+    [SerializeField]
+    List<bool> sticks;
+    public List<AudioClip> sounds;
+    string[] drum_type = new string[] { "Kick", "Snare", "Sticks" };
+    List<float> envelopes;
+    List<bool>[] gates = new List<bool>[3];
+    Vector4 adsr_params;
+
     void Start()
     {
+        for(int i = 0; i < sounds.Count; i++)
+        {
+            //send sound files names to patch
+            //add .wav
+            //drum type is both the name of receive obj 
+            //and of Drums folder subdirectory for sound
+            string name = sounds[i].name + ".wav";
+            patch.SendSymbol(drum_type[i], name);
+            //build list of envelopes
+            envelopes.Add(0);
+        }
+        gates[0] = kick;
+        gates[1] = snare;
+        gates[2] = sticks;
+        adsr_params = new Vector4(100, 50, 0.4f, 200);
     }
 
     // Update is called once per frame
@@ -31,10 +55,21 @@ public class SequencerDrum : MonoBehaviour
             if (kick[count])
             {
                 patch.SendBang("kick_bang");
-
-                count = (count + 1) % kick.Count;
+            } 
+            if (snare[count])
+            {
+                patch.SendBang("snare_bang");
             }
+            if (sticks[count])
+            {
+                patch.SendBang("sticks_bang");
+            }
+            count = (count + 1) % kick.Count;
+        }
 
+        for (int i = 0; i < sounds.Count; i++)
+        {
+            envelopes[i] = ControlFunctions.ADSR(ramp, gates[i][count], adsr_params);
         }
     }
 }
